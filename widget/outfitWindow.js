23a7908d5bc8edf9c3eadbe016f6b8460c8760ff
richhowell07 <richhowell07@gmail.com>
2014-06-09 05:56:52 +0000

    Updated file structure, changed CSS, and now change playerWindows based off of faction.

--- widget/outfitWindow.js
+++ widget/outfitWindow.js
@@ -2,40 +2,37 @@
 	feedInstance.js
 	Holds a feed and determines layout.
 **/
-function Feed (params) {
+function OutfitWindow (params) {
 	var self = this;
 	var tag = params.tag;
 	var wall_id = '#' + params.wall_id;
-	var window_id = '#feed_' + Feed.feedId + '_window';
-	var label_id = '#feed_' + Feed.feedId + '_label';
-	var close_id = '#feed_' + Feed.feedId + '_close';
-	var status_id = '#feed_' + Feed.feedId + '_status';
-	var online_id = '#feed_' + Feed.feedId + '_online';
-	var kd_id = '#feed_' + Feed.feedId + '_kd';
-	var kills_id = '#feed_' + Feed.feedId + '_kills';
-	var deaths_id = '#feed_' + Feed.feedId + '_deaths';
-	var queue_id = '#feed_' + Feed.feedId + '_queue';
+	var window_id = '#feed_' + OutfitWindow.feedId + '_window';
+	var label_id = '#feed_' + OutfitWindow.feedId + '_label';
+	var close_id = '#feed_' + OutfitWindow.feedId + '_close';
+	var status_id = '#feed_' + OutfitWindow.feedId + '_status';
+	var online_id = '#feed_' + OutfitWindow.feedId + '_online';
+	var kd_id = '#feed_' + OutfitWindow.feedId + '_kd';
+	var kills_id = '#feed_' + OutfitWindow.feedId + '_kills';
+	var deaths_id = '#feed_' + OutfitWindow.feedId + '_deaths';
+	var queue_id = '#feed_' + OutfitWindow.feedId + '_queue';
 	var feed = {};
 	
-	_createElements();
+	_createWindow();
+	_createFields();
 	_createOutfitFeed();
 	
-	Feed.feedId++;
+	OutfitWindow.feedId++;
 	
 
 // PRIVATE FUNCTIONS
-	function _createElements(){
-		_createWindow();
-		_createFields();
-	}
 	
 	function _createWindow(){
 
-		console.log("wall_id:",window_id," for tag:",tag);
+		//console.log("wall_id:",window_id," for tag:",tag);
 		$(wall_id).append('<span></span>');
 		$(wall_id + ' span:last').attr("id", window_id.substring(1));
 		$(wall_id + ' span:last').attr("style", "display:inline; float:left; width:250px");
-		console.log("created window_id:", window_id, " for tag:",tag);
+		//console.log("created window_id:", window_id, " for tag:",tag);
 	}
 	
 	function _createFields(){
@@ -94,7 +91,7 @@ function Feed (params) {
 		$(window_id + ' input:last').attr("style", "width:3em");
 		$(window_id + ' input:last').attr("readonly", true);
 	
-		// Feed list
+		// OutfitWindow list
 		$(window_id).append('<div></div>');
 		$(window_id + ' div:last').attr("style", "height:250px; overflow:auto;");
 		$(window_id + ' div:last').append('<table></table>');
@@ -118,34 +115,13 @@ function Feed (params) {
 	}
 	
 	$(close_id).click( function() {
-		// ugly code, but works, should use CSS transforms instead?
-		/*$(window_id +','+window_id+' div' +','+window_id+' input' +','+window_id+' span' +','+window_id+' table').each(function(ix, obj) {
-			$(obj).animate({
-				opacity : 0, 
-				left: '-='+$(obj).width()/4, 
-				top: '-='+$(obj).height()/4,
-				height:0, 
-				width:0
-			}, 
-			200, 
-			function() { 
-				feed.closeAll();// close connection
-				$(window_id).remove(); 
-			});
-		});
-		$(window_id).animate({
-			'opacity' : 0,
-			'width': 0, 
-			'height': 0,
-			'left': 0 + 'px',
-			'top': 0 + 'px'
-		});*/
+		
 		$(window_id).remove();
 		feed.closeAll();// close connection
 	});
 }
 
-Feed.feedId = 0;
+OutfitWindow.feedId = 0;
 
 
 
--- widget/playerWindow.js
+++ widget/playerWindow.js
@@ -2,46 +2,46 @@
 	feedInstance.js
 	Holds a feed and determines layout.
 **/
-function playerFeedWindow (params) {
+function PlayerWindow (params) {
 	var self = this;
 	var pname = params.pname.toLowerCase();
 	var wall_id = '#' + params.wall_id;
-	var window_id = '#pfeed_' + playerFeedWindow.feedId + '_window';
-	var label_id = '#pfeed_' + playerFeedWindow.feedId + '_label';
-	var close_id = '#pfeed_' + playerFeedWindow.feedId + '_close';
-	var status_id = '#pfeed_' + playerFeedWindow.feedId + '_status';
-	var online_id = '#pfeed_' + playerFeedWindow.feedId + '_online';
-	var kd_id = '#pfeed_' + playerFeedWindow.feedId + '_kd';
-	var kills_id = '#pfeed_' + playerFeedWindow.feedId + '_kills';
-	var deaths_id = '#pfeed_' + playerFeedWindow.feedId + '_deaths';
-	var queue_id = '#pfeed_' + playerFeedWindow.feedId + '_queue';
+	var window_id = '#pfeed_' + PlayerWindow.feedId + '_window';
+	var label_id = '#pfeed_' + PlayerWindow.feedId + '_label';
+	var videoButton_id = '#pfeed_' + PlayerWindow.feedId + '_videoButton';
+	var feedDiv_id = '#pfeed_' + PlayerWindow.feedId + '_feedDiv';
+	var close_id = '#pfeed_' + PlayerWindow.feedId + '_close';
+	var status_id = '#pfeed_' + PlayerWindow.feedId + '_status';
+	var online_id = '#pfeed_' + PlayerWindow.feedId + '_online';
+	var kd_id = '#pfeed_' + PlayerWindow.feedId + '_kd';
+	var kills_id = '#pfeed_' + PlayerWindow.feedId + '_kills';
+	var deaths_id = '#pfeed_' + PlayerWindow.feedId + '_deaths';
+	var queue_id = '#pfeed_' + PlayerWindow.feedId + '_queue';
 	var playerFeed = {};
 	
 	_createElements();
 	_createPlayerFeed();
 	
-	playerFeedWindow.feedId++;
+	PlayerWindow.feedId++;
 	
 
 // PRIVATE FUNCTIONS
 	function _createElements(){
 		_createWindow();
-		_createFields();
 	}
 	
 	function _createWindow(){
 
-		console.log("wall_id:",window_id," for pname:",pname);
+		//console.log("wall_id:",window_id," for pname:",pname);
 		$(wall_id).append('<span></span>');
 		$(wall_id + ' span:last').attr("id", window_id.substring(1));
-		$(wall_id + ' span:last').attr("class", 'window tr');
-		//$(wall_id + ' span:last').attr("style", "display:inline; float:left;");
-		console.log("created window_id:", window_id, " for pname:",pname);
+		$(wall_id + ' span:last').attr("class", 'window');
+		//console.log("created window_id:", window_id, " for pname:",pname);
 	}
 	
-	function _createFields(){
+	function _createFields(data){
 	
-		console.log("window_id:",window_id);
+		console.log("_createFields() data:",data);
 		
 		// outfit label
 		$(window_id).append('<span></span>');
@@ -52,6 +52,12 @@ function playerFeedWindow (params) {
 		// close button
 		$(window_id).append('<button>X</button>');
 		$(window_id + ' button:last').attr("id", close_id.substring(1));
+		$(window_id + ' button:last').attr("class", "window_class");
+		$(window_id + ' button:last').attr("style", "float:right;" );
+		
+		// video button
+		$(window_id).append('<button>V</button>');
+		$(window_id + ' button:last').attr("id", videoButton_id.substring(1));
 		$(window_id + ' button:last').attr("class", "window_class");
 		$(window_id + ' button:last').attr("style", "float:right;" );
 		
@@ -70,6 +76,7 @@ function playerFeedWindow (params) {
 		$(window_id + ' input:last').attr("id", online_id.substring(1));
 		$(window_id + ' input:last').attr("style", "width:7em");
 		$(window_id + ' input:last').attr("readonly", true);
+		$(window_id + ' input:last').val(data.online_status);
 		
 		// kill/death ratio
 		$(window_id).append('<span></span>');
@@ -97,10 +104,18 @@ function playerFeedWindow (params) {
 	
 		// Feed list
 		$(window_id).append('<div></div>');
+		$(window_id + ' div:last').attr("id", feedDiv_id.substring(1));
 		$(window_id + ' div:last').attr("style", "height:250px; overflow:auto;");
 		$(window_id + ' div:last').append('<table></table>');
 		$(window_id + ' table:last').attr("id", queue_id.substring(1));
 		
+		//if( data.online_status == '1' ) {
+			var faction = API.getFactionShort( data.faction_id ) ;
+			//console.log("data.:",data);
+			$(window_id).attr("class", 'window ' + faction );
+		//} else { // offline
+		//	$(window_id).attr("class", 'window offline');
+		//}
 	}
 	
 	function _createPlayerFeed(){
@@ -111,42 +126,38 @@ function playerFeedWindow (params) {
 			close_id: close_id.substring(1),
 			feed_id: queue_id.substring(1),
 			con_id: status_id.substring(1),
-			totalM_id: online_id.substring(1),
+			online_id: online_id.substring(1),
 			kdRatio_id: kd_id.substring(1),
 			kills_id: kills_id.substring(1),
-			deaths_id: deaths_id.substring(1)
+			deaths_id: deaths_id.substring(1),
+			onInfoComplete: function (data) {// 'data' is character info
+				//console.log("playerFeed onInfoComplete() data:",data);
+				_createFields(data);
+				_createButtonHandles();
+			},
+			onSocketOpen: function () {
+				//console.log("playerFeed onSocketOpen()");
+			}
 		});
-	}
+	}	
 	
-	$(close_id).click( function() {
-		// ugly code, but works, should use CSS transforms instead?
-		/*$(window_id +','+window_id+' div' +','+window_id+' input' +','+window_id+' span' +','+window_id+' table').each(function(ix, obj) {
-			$(obj).animate({
-				opacity : 0, 
-				left: '-='+$(obj).width()/4, 
-				top: '-='+$(obj).height()/4,
-				height:0, 
-				width:0
-			}, 
-			200, 
-			function() { 
-				playerFeed.closeAll();// close connection
-				$(window_id).remove(); 
-			});
+	function _createButtonHandles(){
+	
+		$(close_id).click( function() {
+			$(window_id).remove();
+			playerFeed.closeAll();// close connection
 		});
-		$(window_id).animate({
-			'opacity' : 0,
-			'width': 0, 
-			'height': 0,
-			'left': 0 + 'px',
-			'top': 0 + 'px'
-		});*/
-		$(window_id).remove();
-		playerFeed.closeAll();// close connection
-	});
+
+		$(videoButton_id).click( function() {
+			//console.log("videoButton_id).click()");
+			//$(queue_id).remove();
+			//$(feedDiv_id).append('<div id="video_wall2" style="background-color:#EDEDED; "><object type="application/x-shockwave-flash" height="170"  width="250" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=hondadude7" bgcolor="#000000"><param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="always" /><param name="allowNetworking" value="all" /><param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf" /><param name="flashvars" value="hostname=www.twitch.tv&channel=hondadude7&auto_play=true&start_volume=25" /></object>');
+		});
+
+	}	
 }
 
-playerFeedWindow.feedId = 0;
+PlayerWindow.feedId = 0;
 
 
 
