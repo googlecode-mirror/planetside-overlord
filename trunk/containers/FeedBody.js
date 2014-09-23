73c076010403cc4f1efe1d061828679511f92f91
richhowell07@gmail.com <richhowell07@gmail.com@9739bb85-7e16-6e73-43eb-ea53dc766111>
2014-09-23 04:59:18 +0000

    Added prototype scan: Scans all online players on outfits above a certain player size. Added addOutfitVirus that will attempt to add the outfit of any player from a 'Death' event. (Has performance issues as this grows exponentially.

--- trunk/containers/FeedBody.js
+++ trunk/containers/FeedBody.js
@@ -79,19 +79,22 @@ return dojo.declare("ps2.containers.FeedBody", [ dijit._Widget, dijit._Templated
 		//this.addPlyr({ player_name: 'ratch'});
 		//this.addPlyr('Okamiba');
 		this.addOutfit('merc');
-		this.addOutfit('baid');
-		this.addOutfit('666');
+		//this.addOutfit('baid');
+		//this.addOutfit('666');
+		//this.startScan('merc');
 	},
 	
 	addOutfit: function (tag_lower) {
 		var self = this;
 		console.log("FeedBody addOutfit:", tag_lower);
-		
+			
 		var outfit = OutfitMon.create({
-			outfit_tag_lower: tag_lower 
+			outfit_tag_lower: tag_lower,
+			
 		}, this.feed_body);
+		
 		if( tag_lower == 'merc' ) {
-			outfit.setPlyrWindow('ratch');
+			//outfit.setPlyrWindow('ratch');
 		}
 	},
 	
@@ -132,6 +135,127 @@ return dojo.declare("ps2.containers.FeedBody", [ dijit._Widget, dijit._Templated
 		var self = this;
 		console.log("FeedBody addOutfitClick:");
 		this.addOutfit(this.tag_input.value);
+	},
+	
+	// **************** OUTFIT VIRUS ******************
+	// 
+	// Checks each player event for that players outfit
+	// if it doesn't exist in this.allOutfits['outfit']
+	// then it creates an OutfitMon
+	
+	allOutfits: {},
+	addOutfitVirus: function (tag_lower) {
+		var self = this;
+		console.log("FeedBody addOutfit:", tag_lower);
+			
+		if( this.allOutfits[tag_lower] == null ) {
+			var outfit = OutfitMon.create({
+				outfit_tag_lower: tag_lower,
+				onKillEvent: function (event) {
+					var out = outfit;
+					//console.log("FeedBody onKillEvent["+out.outfit_tag_lower+"]:", event);
+					self.checkOutfit(event.victim_stats, event);
+				},
+				onDeathEvent: function (event) {
+					var out = outfit;
+					//console.log("FeedBody onDeathEvent["+out.outfit_tag_lower+"]:", event);
+					self.checkOutfit(event.attacker_stats, event);
+				},
+			}, this.feed_body);
+			this.allOutfits[tag_lower] = outfit;
+			if( tag_lower == 'merc' ) {
+				//outfit.setPlyrWindow('ratch');
+			}
+		}
+	},
+	
+	checkOutfit: function (enemy_stats, event) {
+		//console.log("FeedBody checkOutfit:", enemy_stats);
+		if( enemy_stats != null && enemy_stats.outfit_member != null ) {
+			var tag = enemy_stats.outfit_member.alias_lower;
+			this.addOutfitVirus(tag);
+			this.printDebug();
+		} else {// tk causes issue here
+			//console.warn("FeedBody checkOutfit:", event);
+		}
+	},
+	
+	printDebug: function () {
+		console.log("FeedBody printDebug");
+		
+		var debug = dojo.byId('debug_left_pane');
+		debug.innerHTML = '';
+		
+		for(var tag in this.allOutfits) {
+			if( this.allOutfits.hasOwnProperty( tag ) ) {
+				//console.log("FeedBody printDebug2",this.allOutfits[tag].members);
+				
+				// THIS LINE GETS ERROR ON .members is undefined
+				var str = '['+tag+'] '+this.allOutfits[tag].members.onlineCount;
+				
+				debug.innerHTML = debug.innerHTML + '<br>' + str;
+			}
+		}
+	},
+	
+	
+	
+	// **************** SCANNER ******************
+	// Starts looking for all online players
+	// First gets outfits with member count above a certain threshhold
+	// Then counts the number of online players
+	allOutfits: {},
+	startScan: function (tag_lower) {
+		var self = this;
+		console.log("FeedBody addOutfitClick:");
+		
+		
+		self.myGet = Script.get({
+			url: 'http://census.soe.com/s:rch/get/ps2:v2/outfit'
+			+'?member_count=]'+50
+			+'&c:limit=2000'
+			+'&c:resolve=member_online_status()',
+			handleAs: 'json',
+			callbackParamName: "callback",
+		}).then(function (data) {
+			console.log("startScan GET :", data);
+			console.log("startScan GET num outfits:", data.outfit_list.length);
+			
+			var totalOnline = 0;
+			for( var i = 0; i < data.outfit_list.length; ++i ) {
+				if( data.outfit_list[i].members != null ) {
+					totalOnline += self.getOnlineCount(data.outfit_list[i].members);
+				} else {
+					console.warn("startScan GET NO MEMBERS for :", data.outfit_list[i]);
+				}
+			}
+			console.log("startScan GET total:", totalOnline);
+		});
+		
+		// create outfit and put into allOutfits
+		/*self.allOutfits[tag_lower.toLowerCase()] = OutfitMon.create({
+			outfit_tag_lower: tag_lower.toLowerCase(),
+			onComplete: function (data) {
+				console.log("Outfit onComplete:", data.alias, "data", data);
+				
+			}
+		}, this.feed_body);*/
+	},
+	
+	
+	getOnlineCount: function (members) {
+		var self = this;
+		var count = 0;
+	
+		
+		for(var i = 0; i < members.length; ++i) {
+			//console.log('for', members[i]);
+			
+			if( members[i].online_status != '0' ) {
+				count++;
+			}
+		}
+		return count;
 	},
 	
 });
